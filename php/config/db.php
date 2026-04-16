e1ad38c90cddc26858008d2413ff6cb9d7addd6d
arsema619 <arsemamenberu16@gmail.com>
2026-04-16 10:24:56 -0700

    Update db.php

--- php/config/db.php
+++ php/config/db.php
@@ -4,14 +4,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
-try{
-$conn = new PDO (
-   "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']};charset=utf8mb4",
-    $_ENV['DB_USER'],
-    $_ENV['DB_PASS']
-   
-);   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-    die("Connection failed: " .$e->getMessage());
+
+try {
+    $conn = new PDO(
+        "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']};charset=utf8mb4",
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASS']
+    );
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
-?>
